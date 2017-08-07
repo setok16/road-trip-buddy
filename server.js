7110d368f92b3c394cf653c1a2e3a4179c1c0e6e
Austin Corley <arcorley528@gmail.com>
2017-08-07 13:23:50 -0700

    Update server.js

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.get('/landing', function(req,res,next){
 app.post('/authenticate',function(req,res,next){
   var pass = req.body.password;
 
-  mysql.pool.query('SELECT user_nm, password FROM users WHERE user_nm = ?', [req.body.user_nm], function(err, rows, fields){
+  mysql.pool.query('SELECT user_nm, password, user_id FROM users WHERE user_nm = ?', [req.body.user_nm], function(err, rows, fields){
     if(err){
       next(err);
       return;
@@ -54,6 +54,7 @@ app.post('/authenticate',function(req,res,next){
   if (authPass[0] !== undefined){
 	  if(pass === authPass[0].password){
 		req.session.name = authPass[0].user_nm;
+		req.session.userID = authPass[0].user_id;
 		req.session.save();
 	    res.status(200).send();
 	  } 
@@ -79,6 +80,32 @@ app.post('/add_user', function(req,res,next){
 		}
 	});
 
+	res.send();
+});
+
+/***********YOUR PREFERENCES PAGE**********/
+app.get('/preferences', function(req,res,next){
+	if (req.session.userID){
+		var context = {};
+		context.userID = req.session.userID;
+		res.render('preferences', context); //if the user has a session, direct to landing
+	}
+	else {
+		res.redirect('/'); //if the user doesn't have a session, redirect to login page
+	}
+});
+
+/***********ADD PREFERENCES****************/
+// We must make a table called preferences in our db with the below bool fields and a userID field.
+app.post('/add_preferences', function(req,res,next) {
+	mysql.pool.query("UPDATE users SET hikes=?, tpark=?, museum=?, mall=?, npark=?, sports=?, wpark=?, landmark=?, musicEvent=? WHERE user_id=?",
+	[req.body.NatureHikes, req.body.ThemeParks, req.body.Museum, req.body.ShoppingMalls, req.body.NatureParks, req.body.SportingEvents, req.body.WaterPark, req.body.FamousLandmarks, req.body.MusicEvents, req.body.userID],
+	function(err, result) {
+		if(err) {
+			next(err);
+			return;
+		}
+	});
 	res.send();
 });
 
